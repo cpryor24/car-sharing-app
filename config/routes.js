e4beaf4427edcfd191282cb966093222c5248c76
Chris Pryor <chrispryor@live.com>
2018-10-09 15:00:45 -0700

    add vehicle page

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,14 @@
 const main = require("../controllers/main.js")
+const vehicle = require("../controllers/vehicle.js")
 module.exports = function(app){
 
   app.get('/', main.index);
   app.get('/sign-up', main.show);
   app.get('/login', main.login);
   app.post('/sign-up', main.signUp)
+
+  // View Vehicle
+  app.get('/rental/:id', vehicle.rental)
 
   app.use(authenication);
 
